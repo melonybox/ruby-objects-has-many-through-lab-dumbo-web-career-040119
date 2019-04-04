6de76417c5a67d9524d7407d8522a55c846764f9
Gino Desangles <blushingbox@yahoo.com>
2019-04-04 23:46:16 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Genre
+  @@all = []
+  
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def songs
+    song_list = Artist.songs
+    
+    song_list.collect! do |x|
+      x.genre == self
+    end
+  end
+  
+end
